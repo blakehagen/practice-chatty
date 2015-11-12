b5033b715d7f7a47ee070285605053fbea62d386
Blake Hagen <blakehagen@gmail.com>
2015-11-12 15:59:55 -0700

    done

--- server.js
+++ server.js
@@ -1,1 +1,56 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+
+app.use(bodyParser.json());
+
+
+var messages =[{message: "Hello! Type your message!", time: 'right now'}];
+
+
+// GET ENPOINT //
+app.get('/', function (req, res) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+
+app.options('/', function (req, res) {
+    res.status(200).set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }).send();
+});
+
+// POST ENDPOINT //
+
+var date = function(){
+    return moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+
+app.post('/', function (req, res) {
+    messages.push({
+        message: req.body.message,
+        time: new Date()
+    })
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+
+
+app.listen(3000);
